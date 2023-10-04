3e23942a2f5419dddf3ec0f0b6a30bd8d9d6edec
sashika20643 <sashikadulaj20643@gmail.com>
2023-10-04 18:08:35 +0530

    Summerization added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
-
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 
 def search_and_summarize(query):
     # Google Search API key and custom search engine ID
@@ -17,11 +19,19 @@ def search_and_summarize(query):
         title = result.get('title', '')
         url = result.get('link', '')
         snippet = result.get('snippet', '')
-        print(title);
         results_info.append(f"{title}\n{url}\n{snippet}\n\n")
 
     # Generate a summary
-   
+    full_text = '\n'.join(results_info)
+    parser = PlaintextParser.from_string(full_text, Tokenizer('english'))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, 3)  # You can adjust the number of sentences in the summary
+
+    # Display the summary
+    print("Search Results Summary:")
+    for sentence in summary:
+        print(sentence)
+
 # Example usage
 search_and_summarize("programming languages")
 
